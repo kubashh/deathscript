df69b12b945116fb0a35e52dc4e4fad46ea9695a
Jakub <jakubhanula0@gmail.com>
2025-02-12 16:58:19 +0100

    0.0.1.1

---
+++ README.md
@@ -0,0 +1,21 @@
+# DeathSctipt
+Simple interpreted programming language (for now don't working)
+
+
+## 1. Run method
+
+1. Run
+```bash
+  sh run.sh fileName
+```
+
+
+## 2. Run method
+
+1. Build
+zig build
+
+2. Run
+```bash
+  ./zig-out/deathscript fileName
+```
--- src/main.zig
+++ src/main.zig
@@ -3,7 +3,14 @@ const std = @import("std");
 pub fn main() !void {
     std.debug.print("Hello DeathScript!\n", .{});
     std.debug.print("There are {d} args:\n", .{std.os.argv.len});
-    for (std.os.argv) |arg| {
-        std.debug.print("  {s}\n", .{arg});
+    if (std.os.argv.len < 2) {
+        std.debug.print("No args. give file name!\n", .{});
+        return;
     }
+
+    const absPath = std.os.argv[0];
+    const filePath = std.os.argv[1];
+
+    std.debug.print("Abs path: {s}\n", .{absPath});
+    std.debug.print("File name: {s}\n", .{filePath});
 }
